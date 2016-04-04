a1b2bcb09c9926ae4fcd61fca22da7f16532ef34
李家优 <lijiayouyj@163.com>
2016-04-04 23:58:10 +0800

    no message

--- src/javascripts/common/base.js
+++ src/javascripts/common/base.js
@@ -1,6 +1,5 @@
 import '../vendor/nunjucks.js'
-import '../vendor/zepto.js'
-import '../vendor/sm.js'
+import '../vendor/jquery.js'
 
 class Base {
     constructor () {
@@ -15,18 +14,6 @@ class Base {
           }
         })
         
-        $.config = {
-            // 路由功能开关过滤器，返回 false 表示当前点击链接不使用路由
-            routerFilter: function($link) {
-                // 某个区域的 a 链接不想使用路由功能
-                if ($link.is('.disable-router a')) {
-                    return false;
-                }
-                return true;
-            }
-        }
-
-        $.init()
     }
 }
 
